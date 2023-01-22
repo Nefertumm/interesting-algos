be32ceb379048039dbb79e0a60a72b2547f7012d
Nefertumm <nefertum.dev@protonmail.com>
2023-01-22 07:24:40 -0300

    Init: Sieve of Eratosthenes

---
+++ sieve_of_eratosthenes.py
@@ -0,0 +1,27 @@
+import numpy as np
+import time as tm
+
+def sieve_of_eratosthenes(n : int) -> list:
+    """ Find all prime numbers between 2 and a number n
+    Intended to be O(n*log(log n))
+    Source: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+    Args:
+        n (int): top integer limit
+
+    Returns:
+        list: prime numbers between 2 and n
+    """
+    consecutive_numbers : list[bool] = [True for _ in range(n)]
+    for i in range(2, int(np.sqrt(n) + 1)):
+        if consecutive_numbers[i]:
+            for j in range(i*i, n, i):
+                consecutive_numbers[j] = False
+    
+    return [idx for idx, val in enumerate(consecutive_numbers) if val and idx >= 2]
+
+n = input('N: ')
+start = tm.perf_counter_ns()
+primes = sieve_of_eratosthenes(n)
+end = tm.perf_counter_ns()
+print(primes, "\nSize: ", len(primes), 'Elapsed: ', end - start)
